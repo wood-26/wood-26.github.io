315bf819a2cfde2eb8c0e35df5d77efd2f921e66
wood-26 <senaihabtemariam49@gmail.com>
2026-03-09 12:03:42 -0400

    added unfinished README

---
+++ projects/game/README.md
@@ -0,0 +1,3 @@
+# The Mall
+
+The setting takes place in a mall in which the player has to find someone else in thirty minutes. Two minutes will pass with each option taken.  If they don't do that in that timespan, they will fail and have the option to retry.
--- projects/game/game.js
+++ projects/game/game.js
@@ -10,14 +10,17 @@ let gameActive = true; //this variable is required.
 //Make one function for each location
 function locationA() {
     clear();
-    print("\nYou are in location A!");
+    print("\nYou are in the first floor!");
     print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tlocationB");
+        "\n\tClothing store" + "\n\tBook store" + "\n\t2nd Floor");
     
     function processInput(input){
-        if (input.toLowerCase() === "locationb") {
+        if (input.toLowerCase() === "clothing store") {
             locationB();
-        } else {
+        } 
+        else if (input.toLowerCase() === "book store") {
+            locationC(); }
+        else {
             stayHere();
             waitThenCall(locationA);
         }
@@ -27,12 +30,12 @@ function locationA() {
 
 function locationB() {
     clear();
-    print("\nYou are in location B!");
+    print("\nYou are in the clothing store!");
     print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tlocationA");
+        "\n\tFirst floor");
     
     function processInput(input){
-        if (input.toLowerCase() === "locationa") {
+        if (input.toLowerCase() === "first floor") {
             locationA();
         } else {
             stayHere();
@@ -42,11 +45,16 @@ function locationB() {
     waitForInput(processInput);
 }
 
+function locationC() {
+    clear();
+    print("\nYou are in the book store!");
+    print("\nWhere do you want to go next? Say one of these choices:" + "\n\tFirst floor");
+}
 //finally, make sure you customize this to tell it what should happen at the
 //very start. For this simple example, any input will bring you
 //to locationA
 function start(){
-    print("Welcome to my game! Press any key to start");
+    print("Welcome to my game! Press any key to get off the bus");
 
     function processInput(input){
             locationA();
