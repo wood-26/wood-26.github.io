1809669ecf487b34dc6da7dfbc079395e007e91f
wood-26 <senaihabtemariam49@gmail.com>
2026-03-26 12:51:45 -0400

    did first 3

---
+++ practice/loops/loops.js
@@ -0,0 +1,39 @@
+let canvas = document.getElementById("canvas1");
+let ctx = canvas.getContext("2d");
+
+//fill in code for canvas1 here
+for (let y=0; y<=300;y+=150){
+for (let x=0; x<800; x+=100){
+    ctx.fillRect(x,y,50,20);   
+    }
+}
+
+canvas = document.getElementById("canvas2");
+ctx = canvas.getContext("2d");
+
+//fill in code for canvas2 here
+
+for (let x=0;x<=800;x+=150){
+for (let y=100;y<=800;y+=150){
+    if(y>x){
+        ctx.fillRect(x,y,50,20);
+    }
+}
+}
+canvas = document.getElementById("canvas3");
+ctx = canvas.getContext("2d");
+
+//fill in code for canvas3 here
+
+for (let x=100; x<=800; x+=150){
+for (let y=100; y<800; y+=150){
+    if (y!=x){
+        ctx.fillRect(x,y,50,50);
+    }
+}
+}
+
+canvas = document.getElementById("canvas4");
+ctx = canvas.getContext("2d");
+
+//fill in code for canvas4 here
